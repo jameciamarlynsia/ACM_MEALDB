822c30a4d26175d57d5d7aba807c9f40dddfc2d3
jameciamarlynsia <jameciamoore16@gmail.com>
2021-09-09 18:54:12 -0400

    Enter

--- script.js
+++ script.js
@@ -8,25 +8,25 @@ const mealsEl = document.getElementById('meals');
 const singleMealEl = document.getElementById('single-meal');
 
 //////////////
-// FUNCTIONS
+// FUNCTIONSS
 
 // Get JSON
-const getJSON = async function (url, errorMsg = 'Something went wrong') {
-  return await fetch(url).then((response) => {
-    if (!response.ok) {
-      throw new Error(`${errorMsg} (${response.status})`);
-    }
-    return response.json();
-  });
+const getJSON = async function(url, errorMsg = 'Something went wrong') {
+    return await fetch(url).then((response) => {
+        if (!response.ok) {
+            throw new Error(`${errorMsg} (${response.status})`);
+        }
+        return response.json();
+    });
 };
 
 // Add multiple meals to the DOM
-const displayMeals = function (mealDataArr) {
-  const innerHTML = [];
-  mealDataArr.forEach((mealData) => {
-    const string = mealData.meals
-      .map(
-        (meal) => `
+const displayMeals = function(mealDataArr) {
+    const innerHTML = [];
+    mealDataArr.forEach((mealData) => {
+        const string = mealData.meals
+            .map(
+                (meal) => `
   <div class="meal">
     <img src="${meal.strMealThumb}" alt=${meal.strMeal}>
     <div class="meal-info" data-mealID="${meal.idMeal}"> 
@@ -34,96 +34,96 @@ const displayMeals = function (mealDataArr) {
     </div>
   </div>
   `
-      )
-      .join('');
-    innerHTML.push(string);
-  });
-  mealsEl.innerHTML = innerHTML.join('');
+            )
+            .join('');
+        innerHTML.push(string);
+    });
+    mealsEl.innerHTML = innerHTML.join('');
 };
 
 // Search meals & fetch from API
-const searchMeal = async function (e) {
-  e.preventDefault();
-  if (searchBox.value === '') {
-    alert('Please enter a search term');
-    return;
-  }
-  const term = searchBox.value;
-  const mealsArr = [];
-  try {
-    //Search meal by name
-    const mealData = await getJSON(
-      `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
-    );
-    console.log(mealData);
-    if (mealData.meals !== null) mealsArr.push(mealData);
-    // Search by main ingredient
-    const mealData2 = await getJSON(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${term}`
-    );
-    if (mealData2.meals !== null) mealsArr.push(mealData2);
-    // Filter by category
-    const mealData3 = await getJSON(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${term}`
-    );
-    console.log(mealData3);
-    if (mealData3.meals !== null) mealsArr.push(mealData3);
-    // Filter by area
-    const mealData4 = await getJSON(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?a=${term}`
-    );
-    if (mealData4.meals !== null) mealsArr.push(mealData4);
-
-    //Create heading for result
-    resultHeading.innerHTML = `<h2> Search results for '${term}':</h2>`;
-    if (mealsArr.length === 0) {
-      //If no results
-      resultHeading.innerHTML = `<p> No search results for '${term}'. Please try again!</p>`;
-      mealsEl.innerHTML = '';
-      if (term === 'desert')
-        resultHeading.innerHTML = `<p> No search results for '${term}'. Try "dessert"`;
-      return;
-    } else {
-      // Add the meal data to the DOM
-      displayMeals(mealsArr);
+const searchMeal = async function(e) {
+    e.preventDefault();
+    if (searchBox.value === '') {
+        alert('Please enter a search term');
+        return;
     }
-    //Clear text area
-    searchBox.value = '';
-  } catch (err) {
-    alert(err.message);
-  }
+    const term = searchBox.value;
+    const mealsArr = [];
+    try {
+        //Search meal by name
+        const mealData = await getJSON(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
+        );
+        console.log(mealData);
+        if (mealData.meals !== null) mealsArr.push(mealData);
+        // Search by main ingredient
+        const mealData2 = await getJSON(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?i=${term}`
+        );
+        if (mealData2.meals !== null) mealsArr.push(mealData2);
+        // Filter by category
+        const mealData3 = await getJSON(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?c=${term}`
+        );
+        console.log(mealData3);
+        if (mealData3.meals !== null) mealsArr.push(mealData3);
+        // Filter by area
+        const mealData4 = await getJSON(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?a=${term}`
+        );
+        if (mealData4.meals !== null) mealsArr.push(mealData4);
+
+        //Create heading for result
+        resultHeading.innerHTML = `<h2> Search results for '${term}':</h2>`;
+        if (mealsArr.length === 0) {
+            //If no results
+            resultHeading.innerHTML = `<p> No search results for '${term}'. Please try again!</p>`;
+            mealsEl.innerHTML = '';
+            if (term === 'desert')
+                resultHeading.innerHTML = `<p> No search results for '${term}'. Try "dessert"`;
+            return;
+        } else {
+            // Add the meal data to the DOM
+            displayMeals(mealsArr);
+        }
+        //Clear text area
+        searchBox.value = '';
+    } catch (err) {
+        alert(err.message);
+    }
 };
 
 // Class to work with the API format
 class recipeItem {
-  constructor(ingredient, quantity) {
-    this.ingredient = ingredient;
-    this.quantity = quantity;
-  }
+    constructor(ingredient, quantity) {
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+    }
 }
 
 // Add the selected meal to the DOM
-const addMealToDOM = function (meal) {
-  //Get recipe data from the object
-  const ingredients = [];
-  const measurements = [];
-  const recipeData = [];
-  //the API has data for recipe in a goofy way
-  for (let key in meal) {
-    let word = key;
-    if (word.includes('strIngredient') && meal[key] !== '') {
-      ingredients.push(meal[key]);
-    }
-    if (word.includes('strMeasure') && meal[key] !== '') {
-      measurements.push(meal[key]);
-    }
-  }
-  for (let i = 0; i < ingredients.length; i++) {
-    let j = new recipeItem(ingredients[i], measurements[i]);
-    recipeData.push(j);
-  }
-  // Add meal data to the DOM
-  singleMealEl.innerHTML = `
+const addMealToDOM = function(meal) {
+        //Get recipe data from the object
+        const ingredients = [];
+        const measurements = [];
+        const recipeData = [];
+        //the API has data for recipe in a goofy way
+        for (let key in meal) {
+            let word = key;
+            if (word.includes('strIngredient') && meal[key] !== '') {
+                ingredients.push(meal[key]);
+            }
+            if (word.includes('strMeasure') && meal[key] !== '') {
+                measurements.push(meal[key]);
+            }
+        }
+        for (let i = 0; i < ingredients.length; i++) {
+            let j = new recipeItem(ingredients[i], measurements[i]);
+            recipeData.push(j);
+        }
+        // Add meal data to the DOM
+        singleMealEl.innerHTML = `
   <div class="single-meal">
     <h1>${meal.strMeal}</h1>
     <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
